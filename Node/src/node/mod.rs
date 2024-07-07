38455b2359d2c453a7d9af11381909728d315fed
Maciej Skrzypkowski <mskr@gmx.com>
2024-07-07 10:13:11 +0200

    sync ethcc-testnet with master (#48)
    * RPC client for fetching tx lists (#23)
    
    * RPC client for fetching tx lists
    
    RPC server for tests
    
    * Improve async processing and error handling
    
    * More assert to  the gen l2 tx test
    
    ---------
    
    Co-authored-by: Denis Kolodin <deniskolodin@gmail.com>
    Co-authored-by: Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>
    
    * submit new l2 blocks using RPC (#28)
    
    * submit_new_l2_blocks implemented
    
    * fixed tests
    
    * Fix loops in Node/src/node/mod.rs to be consistent (#38)
    
    every 4 seconds and not blocked by channel
    processing
    
    Co-authored-by: Ahmad Bitar <amb014@diyarme.com>
    
    * Two RCP addresses for the Node
    
    fixed some JSON RPC compatibility with taiko-clients
    
    * unused import
    
    * small fixes
    
    ---------
    
    Co-authored-by: Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>
    Co-authored-by: Ahmad Bitar <amb014@diyarme.com>
    
    * Unit test for deployment and call of Ethereum contract (#39)
    
    * Unit test for deployment and call of Ethereum contract
    
    * foundry for github actions
    
    * propose_new_block call with empty parameters
    
    * Default params for the newBlockProposal
    
    * rest parameter passed to the propose_new_block
    
    * Send new block proposal by raw tx (#47)
    
    * build Eip1559 transaction
    
    * Handling additional arguments passed by JSON RPC from proposer
    
    * Forwarding of encoded tx to the mev boost module
    
    * Configuration parameters take form the ENV
    
    * sending new block proposal using builder.send
    
    * renamed according to the review
    
    ---------
    
    Co-authored-by: Denis Kolodin <deniskolodin@gmail.com>
    Co-authored-by: Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>
    Co-authored-by: Ahmad Bitar <amb014@diyarme.com>

--- Node/src/node/mod.rs
+++ Node/src/node/mod.rs
@@ -1,53 +1,41 @@
-use crate::taiko::Taiko;
+use crate::{ethereum_l1::EthereumL1, mev_boost::MevBoost, taiko::Taiko};
+use anyhow::{anyhow as any_err, Error, Ok};
 use tokio::sync::mpsc::{Receiver, Sender};
 
 pub struct Node {
     taiko: Taiko,
     node_rx: Option<Receiver<String>>,
     avs_p2p_tx: Sender<String>,
+    gas_used: u64,
+    ethereum_l1: EthereumL1,
+    _mev_boost: MevBoost, // temporary unused
 }
 
 impl Node {
-    pub fn new(node_rx: Receiver<String>, avs_p2p_tx: Sender<String>) -> Self {
-        let taiko = Taiko::new();
+    pub fn new(
+        node_rx: Receiver<String>,
+        avs_p2p_tx: Sender<String>,
+        taiko: Taiko,
+        ethereum_l1: EthereumL1,
+        mev_boost: MevBoost,
+    ) -> Self {
         Self {
             taiko,
             node_rx: Some(node_rx),
             avs_p2p_tx,
+            gas_used: 0,
+            ethereum_l1,
+            _mev_boost: mev_boost,
         }
     }
 
     /// Consumes the Node and starts two loops:
     /// one for handling incoming messages and one for the block preconfirmation
-    pub fn start(mut self) {
+    pub async fn entrypoint(mut self) -> Result<(), Error> {
         tracing::info!("Starting node");
         self.start_new_msg_receiver_thread();
-        self.main_block_preconfirmation_loop()
-    }
-
-    fn main_block_preconfirmation_loop(&self) {
-        loop {
-            self.taiko.get_pending_l2_tx_lists();
-            self.commit_to_the_tx_lists();
-            self.send_preconfirmations_to_the_avs_p2p();
-            self.taiko.submit_new_l2_blocks();
-
-            //TODO: remove after implementation of above methods
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        }
-    }
-
-    fn commit_to_the_tx_lists(&self) {
-        //TODO: implement
-    }
-
-    fn send_preconfirmations_to_the_avs_p2p(&self) {
-        let avs_p2p_tx = self.avs_p2p_tx.clone();
-        tokio::spawn(async move {
-            if let Err(e) = avs_p2p_tx.send("Hello from node!".to_string()).await {
-                tracing::error!("Failed to send message to avs_p2p_tx: {}", e);
-            }
-        });
+        self.preconfirmation_loop().await;
+        Ok(())
     }
 
     fn start_new_msg_receiver_thread(&mut self) {
@@ -65,8 +53,55 @@ impl Node {
             tokio::select! {
                 Some(message) = node_rx.recv() => {
                     tracing::debug!("Node received message: {}", message);
-                }
+                },
             }
         }
+    }
+
+    async fn preconfirmation_loop(&self) {
+        loop {
+            let start_time = tokio::time::Instant::now();
+            if let Err(err) = self.main_block_preconfirmation_step().await {
+                tracing::error!("Failed to execute main block preconfirmation step: {}", err);
+            }
+            let elapsed = start_time.elapsed();
+            let sleep_duration = std::time::Duration::from_secs(4).saturating_sub(elapsed);
+            tokio::time::sleep(sleep_duration).await;
+        }
+    }
+
+    async fn main_block_preconfirmation_step(&self) -> Result<(), Error> {
+        let pending_tx_lists = self
+            .taiko
+            .get_pending_l2_tx_lists()
+            .await
+            .map_err(Error::from)?;
+        if pending_tx_lists.tx_list_bytes.is_empty() {
+            return Ok(());
+        }
+
+        self.commit_to_the_tx_lists();
+        self.send_preconfirmations_to_the_avs_p2p().await?;
+        self.taiko
+            .advance_head_to_new_l2_block(pending_tx_lists.tx_lists, self.gas_used)
+            .await?;
+        self.ethereum_l1
+            .propose_new_block(
+                pending_tx_lists.tx_list_bytes[0].clone(), //TODO: handle rest tx lists
+                pending_tx_lists.parent_meta_hash,
+            )
+            .await?;
+        Ok(())
+    }
+
+    fn commit_to_the_tx_lists(&self) {
+        //TODO: implement
+    }
+
+    async fn send_preconfirmations_to_the_avs_p2p(&self) -> Result<(), Error> {
+        self.avs_p2p_tx
+            .send("Hello from node!".to_string())
+            .await
+            .map_err(|e| any_err!("Failed to send message to avs_p2p_tx: {}", e))
     }
 }
---
+++ Node/src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod config;
+pub mod rpc_client;
+pub mod rpc_server;
